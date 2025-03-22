7c33fb0fee592b05bb555c7f7cfa1bdd64fb5f74
Zachary Malinka <zacharymalinka@gmail.com>
2025-03-21 21:40:26 -0400

    production mode

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
-const PORT = 3000; //dev
-// const PORT = 80; //prod
+// const PORT = 3000; //dev
+const PORT = 80; //prod
 
 app.listen(PORT, () => {
   console.log(`I am listening on port ${PORT}`);
